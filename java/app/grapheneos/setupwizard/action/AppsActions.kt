68b6641685a371f2daa7441cf48550ef5956e87c
_______166 <retromac9x2@gmail.com>
2024-02-24 19:41:57 +0000

    Revert "Update AppsActions.kt"
    This reverts commit ec8e1cbebc1a3691cba12edf39b5686f3fc59e80.

--- java/app/grapheneos/setupwizard/action/AppsActions.kt
+++ java/app/grapheneos/setupwizard/action/AppsActions.kt
@@ -15,7 +15,6 @@ object AppsActions {
     fun launchMigration(context: SetupWizardActivity) {
         Log.d(TAG, "launchAppInstaller")
         val intent = Intent(ACTION_APP_INSTALL)
-        intent.putExtra("SuW", true)
         SetupWizard.startActivityForResult(context, intent)
     }
 
